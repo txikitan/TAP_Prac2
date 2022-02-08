c03ce7977f66770ad6b15725ab57daf1eaa21611
Gabriel Garcia <gabriel.garcia@estudiants.urv.cat>
2022-02-08 09:40:57 +0100

    Finished implementing the main object to show the features

--- Scala/ListFilterMap.scala
+++ Scala/ListFilterMap.scala
@@ -1,7 +1,7 @@
 /*-------------------------------------------------------------
 - TAP JavaDataFrame: Scala implementation of the listFilterMap method
     using the two paradigms of recursion
-    
+
     Gabriel Garcia
 /-------------------------------------------------------------*/
 import scala.collection.mutable.ListBuffer
@@ -12,18 +12,17 @@ class ListFilterMap {
   def listFilterMapStack[A,B](condition: A=>Boolean, operation:A=>B, collection:List[A]):List[B] = {
     collection match {
       case Nil => Nil
-      case x :: xs => if(condition(x)) { //filter
-        operation(x)::listFilterMapStack(condition, operation, xs) // apply de operation and then recursive call
+      case x :: xs => if(condition(x)) { //filter with the condition
+        operation(x)::listFilterMapStack(condition, operation, xs) // apply the operation and then recursive call
       } else listFilterMapStack(condition,operation,xs)
     }
   }
   /*Tail recursion*/
-  def listFilterMapTail[A,B](condition: A=>Boolean, operation:A=>B, collection:List[A], accum:ListBuffer[B]): List[B] = {
+  def listFilterMapTail[A,B](condition: A=>Boolean, operation:A=>B, collection:List[A], accum:List[B]): List[B] = {
     collection match {
-      case Nil => accum.toList    // finished working with the list: return the accumulated result
-      case x::xs => if(condition(x)) {
-        accum+=operation(x) // update the accumulator if filtered
-        listFilterMapTail(condition, operation, xs, accum) // recursive call
+      case Nil => accum    // finished working with the list: return the accumulated result
+      case x::xs => if(condition(x)) { //filter with the condition
+        listFilterMapTail(condition, operation, xs, accum.appended(operation(x))) // recursive call with the accumulated value operated
        }
         else listFilterMapTail(condition,operation,xs, accum)
     }
--- Scala/Main.scala
+++ Scala/Main.scala
@@ -10,49 +10,72 @@ import scala.jdk.CollectionConverters.*
 
 object Main {
   def main(args: Array[String]): Unit = {
-
     /*Demonstration of the composite implemented*/
+    var dir = new Directory("Directory")
+    var subdir = new Directory("Subdirectory")
+    val CSVdf1 = new CSVDataFrame("cities.csv")
+    val CSVdf2 = new CSVDataFrame("cities.csv")
+    val CSVdf3 = new CSVDataFrame("cities.csv")
+    subdir.addChild(CSVdf3)
+    dir.addChild(subdir)
+    dir.addChild(CSVdf1)
+    dir.addChild(CSVdf2)
+    println(dir.at(0,"LatD"))
+    println(dir.size())
+    println(dir.columns())
 
 
     /*Example predicate for the filterVisitor*/
-    val predicate2 : Predicate[String] = {(p:String) =>
+    val predicateVisitor : Predicate[String] = {(p:String) =>
       if (p.matches("[0-9]+")) p.toInt > 45
       else false
     }
     /*Demonstration of the two visitors implemented*/
+    val v = new FilterVisitor()
+    dir.accept(v,"LatD",predicateVisitor)
+    println("Filtered: " + v.elements)
+    val c = new CounterVisitor()
+    dir.accept(c,"LatD",predicateVisitor)
+    println("DataFrame files: " + c.files + " DataFrame dirs: " + c.dirs)
 
-
-
-
-
-    /*Demonstration of the recursive listFilterMap */
-    val CSVdf = new CSVDataFrame("cities.csv")
-    val listJava = CSVdf.getCol("LatD")
-    val listScala = listJava.asScala.toList
 
     /*Condition and operation for the two demonstrations of the listFilterMap*/
     /*Round the values of a float-type column that are greater than a value*/
-    def condition(dato : String): Boolean = {
-      if(Integer.valueOf(dato)>45) true
+    def condition(dato : String): Boolean = { // greater than 45
+      if(dato.toFloat>45) true
       else false
+
     }
     def operation(dato:String) : Int = {
-      Integer.valueOf(dato) * 2
+      val floatVal = dato.toFloat
+      val rounded = floatVal.round
+      rounded
     }
     /*Replace a certain word from a string-type column on the elements that contain that word*/
     def condition2(dato : String): Boolean = {
-      if(Integer.valueOf(dato)>45) true
+      val wordToMatch = "Winchester"
+      if(dato.equalsIgnoreCase(wordToMatch)) true
       else false
     }
-    def operation2(dato:String) : Int = {
-      Integer.valueOf(dato) * 2
+    def operation2(dato:String) : String = {
+      val wordToReplace = "Replaced"
+      wordToReplace
     }
     /*Demonstration of the two implementations of listFilterMap*/
-    val listAccum = new ListBuffer[Int]
+    val CSVdf = new CSVDataFrame("cities.csv")
+    val listJava = CSVdf.getCol("LatD")
+    val listJava2 = CSVdf.getCol("City")
+    val listScala = listJava.asScala.toList
+    val listScala2 = listJava2.asScala.toList
+    val listAccum:List[Int] = List()
     val listFilterMap = new ListFilterMap
     val listTail = listFilterMap.listFilterMapTail(condition, operation, listScala,listAccum)
     val listStack = listFilterMap.listFilterMapStack(condition, operation, listScala)
-    println(listTail.toString())
-    println(listStack.toString())
+    val listTail2 = listFilterMap.listFilterMapTail(condition2, operation2, listScala2,listAccum)
+    val listStack2 = listFilterMap.listFilterMapStack(condition2, operation2, listScala2)
+    println("Result of rounding values using tail recursion: "+ listTail.toString())
+    println("Result of rounding values using stack recursion: "+ listStack.toString())
+    println("Result of replacing values using tail recursion(number of values replaced): "+ listTail2.size)
+    println("Result of replacing values using stack recursion(number of values replaced): "+ listStack2.size)
   }
 }
